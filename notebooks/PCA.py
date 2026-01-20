ee8562fa0af42b4a4f4cca54b038506b1547574c
MPerez08 <119086708+MPerez08@users.noreply.github.com>
2026-01-20 00:11:15 -0800

    Update PCA.py

--- notebooks/PCA.py
+++ notebooks/PCA.py
@@ -1,11 +1,11 @@
 # %%
 #This notebook requires the output from DeSeq_Pipeline.py
 
-# 1. Cleaning up summary stats
+# Cleaning up summary stats
 res = stat_res.results_df
 res_clean = res.dropna(subset=['padj'])
 
-# 2. GENE MAPPING
+# GENE MAPPING
 mg = mygene.MyGeneInfo() #Initializing mygene client
 ensembl_ids = res_clean.index.tolist() #Pulling list of Ensembl ID's
 gene_info = mg.querymany(ensembl_ids, scopes='ensembl.gene', fields='symbol', species='human') #Querying the Database to get Gene ID Symbol (Human Specific)
@@ -13,19 +13,26 @@ gene_info = mg.querymany(ensembl_ids, scopes='ensembl.gene', fields='symbol', sp
 # Creating a Mapping Dictionary {ENSG_ID: Symbol}
 mapping_dict = {item['query']: item.get('symbol', item['query']) for item in gene_info}
 
-# 8. PLOTTING PCA
+# PLOTTING PCA
 plt.figure(figsize=(8, 6))
 for cond in pca_df["Condition"].unique():
     subset = pca_df[pca_df["Condition"] == cond]
     plt.scatter(subset["PC1"], subset["PC2"], label=cond, s=100, edgecolors='white')
 
+# Labeling with variance explained
 plt.xlabel(f"PC1 ({pca.explained_variance_ratio_[0]*100:.1f}%)")
 plt.ylabel(f"PC2 ({pca.explained_variance_ratio_[1]*100:.1f}%)")
-plt.legend()
-plt.title("PCA of RNA-seq Samples")
+
+# Title and Legend
+plt.title("PCA of RNA-seq Samples: MIS vs Control", fontsize=14, pad=15)
+plt.legend("Condition", frameon=True)
+plt.grid(True, linestyle='--', alpha=0.3) # Grid to track coordinates
 plt.show()
 
-# 9a. IDENTIFYING DRIVER GENES (Loadings) ---
+# Saving PCA Plot as a high-quality PNG
+plt.savefig("PCA_MIS_vs_Control.png", dpi=300, bbox_inches='tight')
+
+# IDENTIFYING DRIVER GENES (Loadings)
 loadings = pd.DataFrame(
     pca.components_.T,
     columns=['PC1', 'PC2'],
@@ -35,13 +42,12 @@ loadings = pd.DataFrame(
 loadings['symbol'] = loadings.index.map(mapping_dict)
 
 # Getting top 10 genes for PC1
-
 top_pc1_genes = loadings['PC1'].abs().sort_values(ascending=False).head(10).index
 print("\nTop genes driving PC1 variance:")
 
 print(loadings.loc[top_pc1_genes, ['symbol', 'PC1']])
 
-# 9b PLOT LOADINGS ---
+# PLOT LOADINGS ---
 def plot_top_loadings(loadings, pc='PC1', n=10):
     top_genes = loadings[pc].abs().sort_values(ascending=False).head(n).index
     plt.figure(figsize=(8,6))
@@ -51,4 +57,8 @@ def plot_top_loadings(loadings, pc='PC1', n=10):
     plt.gca().invert_yaxis()
     plt.show()
 
+plot_top_loadings(loadings, pc="PC1")
+
+# Saving Top 10 Genes as a high-quality PNG
+plot_top_loadings(loadings, pc="PC1", save_name="PC1_Loadings.png")
 plot_top_loadings(loadings, pc="PC1")
